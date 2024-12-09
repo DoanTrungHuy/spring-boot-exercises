888c66ede409755b38c541614b4bcc46e1ce74d4
doantrunghuy <doantrunghuy1082k2@gmail.com>
2024-12-09 18:27:09 +0700

    Update Properties

--- backend-service/src/main/java/vn/huy/java/backend_service/BackendServiceApplication.java
+++ backend-service/src/main/java/vn/huy/java/backend_service/BackendServiceApplication.java
@@ -1,13 +1,22 @@
 package vn.huy.java.backend_service;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.beans.factory.annotation.Value;
 
 @SpringBootApplication
 public class BackendServiceApplication {
+
+	@Value("${jwt.secretKey}")
+	private String jwtKey;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendServiceApplication.class, args);
 	}
 
+	@PostConstruct
+	public void Test() {
+		System.out.println("jwtKey: " + jwtKey);
+	}
 }
