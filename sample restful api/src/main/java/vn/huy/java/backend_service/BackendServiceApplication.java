60a5506824a3e4afc5e728dac2133e7ba116984f
doantrunghuy <doantrunghuy1082k2@gmail.com>
2024-12-13 06:10:02 +0700

    Update

--- sample restful api/src/main/java/vn/huy/java/backend_service/BackendServiceApplication.java
+++ sample restful api/src/main/java/vn/huy/java/backend_service/BackendServiceApplication.java
@@ -7,16 +7,7 @@ import org.springframework.beans.factory.annotation.Value;
 
 @SpringBootApplication
 public class BackendServiceApplication {
-
-	@Value("${jwt.secretKey}")
-	private String jwtKey;
-
 	public static void main(String[] args) {
 		SpringApplication.run(BackendServiceApplication.class, args);
-	}
-
-	@PostConstruct
-	public void Test() {
-		System.out.println("jwtKey: " + jwtKey);
 	}
 }
