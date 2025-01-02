66209dc743fd88c2a98a615ca6b6a38114f13118
doantrunghuy <doantrunghuy1082k2@gmail.com>
2025-01-02 07:59:38 +0700

    Update author

---
+++ identity-service/src/main/java/com/example/identity_service/service/RoleService.java
@@ -0,0 +1,51 @@
+package com.example.identity_service.service;
+
+import com.example.identity_service.dto.request.RoleRequest;
+import com.example.identity_service.dto.response.RoleResponse;
+import com.example.identity_service.entity.Permission;
+import com.example.identity_service.entity.Role;
+import com.example.identity_service.mapper.RoleMapper;
+import com.example.identity_service.repository.PermissionRepository;
+import com.example.identity_service.repository.RoleRepository;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class RoleService {
+    RoleRepository roleRepository;
+    PermissionRepository permissionRepository;
+    RoleMapper roleMapper;
+
+    public RoleResponse create(RoleRequest request) {
+        Role role = roleMapper.toRole(request);
+        List<Permission> permission = permissionRepository.findAllById(request.getPermissions());
+        role.setPermissions(new HashSet<>(permission));
+        return roleMapper.toRoleResponse(roleRepository.save(role));
+    }
+
+    public List<RoleResponse> getAll() {
+        List<RoleResponse> roleResponseList = new ArrayList<>();
+        List<Role> roles = roleRepository.findAll();
+
+        for (Role role : roles) {
+            roleResponseList.add(roleMapper.toRoleResponse(role));
+        }
+
+        return roleResponseList;
+    }
+
+    public void delete(String role) {
+        roleRepository.deleteById(role);
+    }
+}
