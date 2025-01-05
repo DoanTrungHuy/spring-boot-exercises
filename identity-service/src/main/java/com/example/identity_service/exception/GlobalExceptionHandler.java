323bfa6aacd582bb5cd21455b8b6bbaa613e3dff
doantrunghuy <doantrunghuy1082k2@gmail.com>
2025-01-06 00:54:33 +0700

    Update custom anno

--- identity-service/src/main/java/com/example/identity_service/exception/GlobalExceptionHandler.java
+++ identity-service/src/main/java/com/example/identity_service/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.example.identity_service.exception;
 
 import com.example.identity_service.dto.request.ApiResponse;
+import jakarta.validation.ConstraintViolation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
@@ -8,8 +9,13 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.Map;
+import java.util.Objects;
+
 @ControllerAdvice
 public class GlobalExceptionHandler {
+    private static final String MIN_ATTRIBUTE = "min";
+
     @ExceptionHandler(value = AppException.class)
     public ResponseEntity<ApiResponse> handlingAppException(AppException exception) {
         ErrorCode errorCode = exception.getErrorCode();
@@ -23,18 +29,31 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ApiResponse> handlingMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
         String enumKey = exception.getFieldError().getDefaultMessage();
         ErrorCode errorCode = ErrorCode.KEY_INVALID;
+        Map<String, Object> attributes = null;
 
         try {
             errorCode = ErrorCode.valueOf(enumKey);
+            var constraintViolation = exception.getBindingResult().getAllErrors()
+                    .getFirst().unwrap(ConstraintViolation.class);
+            attributes = constraintViolation.getConstraintDescriptor().getAttributes();
         }
         catch (IllegalArgumentException exp) {
 
         }
 
         ApiResponse apiResponse = new ApiResponse();
+
         apiResponse.setCode(errorCode.getCode());
-        apiResponse.setMessage(errorCode.getMessage());
+        apiResponse.setMessage(Objects.nonNull(attributes) ?
+                mapAttribute(errorCode.getMessage(), attributes)
+                : errorCode.getMessage());
+
         return ResponseEntity.status(errorCode.getStatusCode()).body(apiResponse);
+    }
+
+    private String mapAttribute(String message, Map<String, Object> attributes) {
+        String minValue = String.valueOf(attributes.get(MIN_ATTRIBUTE));
+        return message.replace("{" + MIN_ATTRIBUTE + "}", minValue);
     }
 
     @ExceptionHandler(value = AccessDeniedException.class)
